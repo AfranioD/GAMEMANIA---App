74d5b187eed95c50310a8e7a8e70c219c3ef87fb
Afranio <afraniodimasdasilva@gmail.com>
2022-03-08 20:24:41 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// function mostrarPopup(){
+//     window.alert("wello Word")
+// }
+
+    
+// function mostrarPopup(){
+    //     window.alert("wello Word")
+    // }
+    
+    $(document).ready(function(){
+    
+        $("#barras").click(function(){
+           if($("#menu").hasClass("#menu-ativo")){
+             $("#menu").removeClass("#menu-ativo")
+           }else{
+               $("#menu").addClass("menu-ativo")
+           }
+          })
+        })
+    
+// 
+       
